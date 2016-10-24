e35958bfd414ae96eb8cd6e02974f1a8242a4ef9
Nathan Johnson <nathan.johnson7@pcc.edu>
2016-10-24 15:19:24 -0700

    print date added

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,10 +1,13 @@
 package edu.pcc.cis.helloworld;
 
+import java.util.Date;
+
 /**
  * Created by Nate on 10/24/2016.
  */
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println(new Date());
     }
 }
